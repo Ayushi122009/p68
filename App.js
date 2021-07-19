bcc75127081e8384b6fe7a4624bf46e579caa98e
Ayushi122009 <75616466+Ayushi122009@users.noreply.github.com>
2021-07-19 19:02:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Facebookscreen from './screens/fb';
+import Instagramscreen from './screens/in';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+export default class App extends React.Component {
+  render(){
+  return(
+  <AppContainer/>
+  )
+   }
+  
+}
+
+const TabNavigator = createBottomTabNavigator({
+Facebook: {screen: Facebookscreen},
+Instagram: {screen: Instagramscreen}
+})
+
+const AppContainer = createAppContainer(TabNavigator)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
